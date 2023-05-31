87ba9cea701d7475549a0adc7e6481a7a23894a3
cmirb <51628604+cmirb@users.noreply.github.com>
2023-05-31 11:33:09 +0200

    created a webpage with an etch-a-sketch

---
+++ script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let rainbowMode = false;
+    const container = document.getElementById("container");
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    function darkenColor(element) {
+        const currentColor = getComputedStyle(element).backgroundColor;
+        const match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)(?:\s*,\s*(\d+(?:\.\d+)?))?\)/.exec(currentColor);
+        const darkerColor = `rgb(${match[1]*0.9}, ${match[2]*0.9}, ${match[3]*0.9})`;
+        return darkerColor;
+    }
+
+    function createCell(size) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `${size}px`;
+        cell.style.height = `${size}px`;
+
+        cell.addEventListener("mouseover", function() {
+            if (rainbowMode) {
+                this.style.backgroundColor = getRandomColor();
+            } else {
+                this.style.backgroundColor = darkenColor(this);
+            }
+        });
+
+        return cell;
+    }
+
+    function createGrid(size) {
+        container.innerHTML = "";
+        const cellSize = 960 / size;
+        for(let i = 0; i < size * size; i++) {
+            const cell = createCell(cellSize);
+            container.appendChild(cell);
+        }
+    }
+
+    function rescaleGrid() {
+        let newSize = prompt("Enter new size");
+        if(newSize && !isNaN(newSize) && newSize <= 100) {
+            createGrid(newSize);
+        } else {
+            alert("Please enter a valid number (1-100)");
+        }
+    }
+
+    document.getElementById("rescale").addEventListener("click", rescaleGrid);
+
+    document.getElementById("rainbow").addEventListener("click", function() {
+        rainbowMode = !rainbowMode;
+        this.textContent = rainbowMode ? "Normal Mode" : "Rainbow Mode";
+    });
+
+    createGrid(16);
+});
