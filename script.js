6affb746b47e3b87db33fa4be3918508319f0a53
cmirb <51628604+cmirb@users.noreply.github.com>
2023-05-31 16:36:33 +0200

    added burger menu on mobile

--- script.js
+++ script.js
@@ -1,71 +1,72 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let rainbowMode = false;
-    let temporaryMode = false;
-    const container = document.getElementById("container");
+    (function() {
+      let rainbowMode = false;
+      let temporaryMode = false;
+      const container = document.getElementById("container");
   
-    function getRandomColor() {
-      const r = Math.floor(Math.random() * 256);
-      const g = Math.floor(Math.random() * 256);
-      const b = Math.floor(Math.random() * 256);
-      return `rgb(${r}, ${g}, ${b})`;
-    }
-  
-    function darkenColor(element) {
-      const currentColor = getComputedStyle(element).backgroundColor;
-      const match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)(?:\s*,\s*(\d+(?:\.\d+)?))?\)/.exec(currentColor);
-      const darkerColor = `rgb(${match[1] * 0.9}, ${match[2] * 0.9}, ${match[3] * 0.9})`;
-      return darkerColor;
-    }
-  
-    function createCell(size) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.style.width = `${size}px`;
-      cell.style.height = `${size}px`;
-  
-      function changeColor() {
-        if (rainbowMode) {
-          cell.style.transition = "background-color 0.3s ease";
-          cell.style.backgroundColor = getRandomColor();
-        } else {
-          const currentColor = getComputedStyle(cell).backgroundColor;
-          const targetColor = darkenColor(cell);
-          cell.style.transition = "background-color 0.3s ease";
-          cell.style.backgroundColor = targetColor;
-        }
-  
-        if (temporaryMode) {
-          setTimeout(() => {
-            cell.style.transition = "background-color 0.3s ease";
-            cell.style.backgroundColor = "";
-          }, 1000);
-        }
+      function getRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
       }
   
-      cell.addEventListener("mouseenter", changeColor);
+      function darkenColor(element) {
+        const currentColor = getComputedStyle(element).backgroundColor;
+        const match = /rgba?\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)(?:\s*,\s*(\d+(?:\.\d+)?))?\)/.exec(currentColor);
+        const darkerColor = `rgb(${match[1] * 0.9}, ${match[2] * 0.9}, ${match[3] * 0.9})`;
+        return darkerColor;
+      }
   
-      // Event listeners for touch events on mobile
-      cell.addEventListener("touchstart", function(event) {
-        event.preventDefault();
-        changeColor();
-      });
+      function createCell(size) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `${size}px`;
+        cell.style.height = `${size}px`;
   
-      cell.addEventListener("touchmove", function(event) {
-        event.preventDefault();
-        const touches = event.changedTouches;
-        for (let i = 0; i < touches.length; i++) {
-          const touch = touches[i];
-          const target = document.elementFromPoint(touch.clientX, touch.clientY);
-          if (target === cell) {
-            changeColor();
+        function changeColor() {
+          if (rainbowMode) {
+            cell.style.transition = "background-color 0.3s ease";
+            cell.style.backgroundColor = getRandomColor();
+          } else {
+            const currentColor = getComputedStyle(cell).backgroundColor;
+            const targetColor = darkenColor(cell);
+            cell.style.transition = "background-color 0.3s ease";
+            cell.style.backgroundColor = targetColor;
+          }
+  
+          if (temporaryMode) {
+            setTimeout(() => {
+              cell.style.transition = "background-color 0.3s ease";
+              cell.style.backgroundColor = "";
+            }, 1000);
           }
         }
-      });
   
-      return cell; 
-    }
+        cell.addEventListener("mouseenter", changeColor);
   
-    function createGrid(size) {
+        // Event listeners for touch events on mobile
+        cell.addEventListener("touchstart", function(event) {
+          event.preventDefault();
+          changeColor();
+        });
+  
+        cell.addEventListener("touchmove", function(event) {
+          event.preventDefault();
+          const touches = event.changedTouches;
+          for (let i = 0; i < touches.length; i++) {
+            const touch = touches[i];
+            const target = document.elementFromPoint(touch.clientX, touch.clientY);
+            if (target === cell) {
+              changeColor();
+            }
+          }
+        });
+  
+        return cell; 
+      }
+  
+      function createGrid(size) {
         container.innerHTML = "";
         const containerStyle = getComputedStyle(container);
         const containerWidth = parseInt(containerStyle.getPropertyValue('width'), 10);
@@ -79,39 +80,27 @@ document.addEventListener("DOMContentLoaded", function() {
           container.appendChild(cell);
         }
       }
-      
   
-    function rescaleGrid() {
-      let newSize = prompt("Enter new size (1-100)");
-      if (newSize && !isNaN(newSize) && newSize <= 100) {
-        createGrid(newSize);
-      } else {
-        alert("Please enter a valid number (1-100)");
-      }
-    }
+      createGrid(16);
   
-    function toggleTemporaryMode() {
-      temporaryMode = !temporaryMode;
-      const button = document.getElementById("temporary-mode");
-      button.textContent = temporaryMode ? "Permanent Mode" : "Temporary Mode";
-    }
-  
-    function eraseAllCells() {
-      const cells = container.querySelectorAll(".cell");
-      cells.forEach(cell => {
-        cell.style.transition = "background-color 0.3s ease";
-        cell.style.backgroundColor = "";
+      document.getElementById("reset-button").addEventListener("click", function() {
+        const newSize = parseInt(prompt("Enter a new grid size"), 10);
+        if (newSize > 0 && newSize <= 100) {
+          createGrid(newSize);
+        } else {
+          alert("Invalid grid size. Please enter a number between 1 and 100.");
+        }
       });
-    }
   
-    document.getElementById("rescale").addEventListener("click", rescaleGrid);
-    document.getElementById("rainbow").addEventListener("click", function() {
-      rainbowMode = !rainbowMode;
-      this.textContent = rainbowMode ? "B&W Mode" : "Rainbow Mode";
-    });
-    document.getElementById("temporary-mode").addEventListener("click", toggleTemporaryMode);
-    document.getElementById("erase-all").addEventListener("click", eraseAllCells);
+      document.getElementById("rainbow-button").addEventListener("click", function() {
+        rainbowMode = !rainbowMode;
+        this.classList.toggle("active");
+      });
   
-    createGrid(16);
+      document.getElementById("temporary-button").addEventListener("click", function() {
+        temporaryMode = !temporaryMode;
+        this.classList.toggle("active");
+      });
+    })();
   });
   
